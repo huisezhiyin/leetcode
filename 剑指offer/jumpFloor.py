c55d58245afaae0a013941d140b4056ee42991c1
grey <837364695@qq.com>
2019-12-08 21:10:13 +0800

    [commit]

---
+++ 剑指offer/jumpFloor.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def jumpFloor(self, number):
+        if number <= 2:
+            return number
+
+        result = 0
+        n, m = 1, 2
+        for i in range(number - 2):
+            result = m + n
+            n, m = m, result
+        return result
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.jumpFloor(4))
