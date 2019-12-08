c6131ea148b925ab2030fc4ec956d3a87b38afff
grey <837364695@qq.com>
2019-12-08 19:37:59 +0800

    [commit]

---
+++ 剑指offer/Fibonacci.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def Fibonacci(self, n):
+        if n in [0, 1]:
+            return n
+        s = 1
+        one = 0
+
+        for index in range(2, n + 1):
+            s = s + one
+            one = s - one
+
+        return s
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.Fibonacci(2))
